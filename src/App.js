9314256fb64ab1acfbdd7c0363483dc0e1cd1e86
Tuananht2k95 <ad0txle2407@gmail.com>
2025-02-14 13:59:05 +0700

    Sửa bg layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { routers } from './router/router.js';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,7 +1,7 @@
 export const Content = (data) => {
     return(
-        <>
-            <div className="content d-flex-column mx-5">
+        <div className="d-flex justify-content-center">
+            <div className="content d-flex-column mx-5 col-11 col-sm-10 col-md-8 col-lg-6">
                 <h3 className="p-2">{data.title}</h3>
                 <img className="d-block mx-auto mb-3" src={data.img} alt={data.title} />
                 {
@@ -10,6 +10,6 @@ export const Content = (data) => {
                     ))
                 }
             </div>
-        </>
+        </div>
     )
 }
